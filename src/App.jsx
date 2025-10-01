1a55901e03f533448e2979b954777539579836ff
Tarliomar <tarliomar.pro@gmail.com>
2025-10-01 15:52:02 -0300

    Adicionando arquivos do projeto inicial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+import Welcome from "./assets/componentes/welcome"; 
+
+function App() {
+  return (
+    <>
+      {/* 6.1 Criação do meu Hello word! */}
+      <Welcome/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/componentes/Welcome.jsx
@@ -0,0 +1,5 @@
+function Welcome(){ 
+    return <h1>Olá mundo! </h1>;
+}
+
+export default Welcome;
